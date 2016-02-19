5fd1558e204b8f5d06352baad23b9ddc3867628e
Tilman Schlenker <tilman.schlenker@avid.com>
2016-02-19 13:58:55 +0100

    Initial Commit

---
+++ DND Test/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DND Test
+//
+//  Created by Tilman Schlenker on 19/02/16.
+//  Copyright © 2016 Tilman Schlenker. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DND Test/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DND Test
+//
+//  Created by Tilman Schlenker on 19/02/16.
+//  Copyright © 2016 Tilman Schlenker. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
